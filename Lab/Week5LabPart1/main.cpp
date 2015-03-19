0861ee64bd3e316a7102e939b6a12a92afbc3891
boogg <tchajagos@hotmail.com>
2015-03-19 14:13:57 -0700

    Signed-off-by: boogg <tchajagos@hotmail.com>

--- Lab/Week5LabPart1/main.cpp
+++ Lab/Week5LabPart1/main.cpp
@@ -15,59 +15,64 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-//    //part 1
-//    double balance, payment, interest, total;
-//    string answer;
-//    cout<<"Part 1\n\n";
-//    do{
-//    cout<<"Enter your Balance:";
-//    cin>>balance;    
-//    cout<<"$"<<setprecision(2)<<fixed<<balance;
-//    if(balance<=1000){
-//    interest = balance *0.015;
-//    }else{
-//    interest = 15 + (balance-1000)*0.01;
-//    }
-//    balance += interest;
-//    payment = balance/10;
-//    if(balance<10 || payment<10){
-//        payment = 10;
-//    }
-//    cout<<" balance = $"<<fixed<<balance<<" total, $"<<payment<<
-//            fixed<<" minimum Payment\n";
-//    cout<<"Calculate again? yes/no: ";
-//    cin>>answer;
-//    }while(answer == "yes");
-//    
+    //part 1
+    double balance, payment, interest, total;
+    string answer;
+    cout<<"Part 1\n\n";
+    do{
+    cout<<"Enter your Balance:";
+    cin>>balance;    
+    cout<<"$"<<setprecision(2)<<fixed<<balance;
+    if(balance<=1000){
+    interest = balance *0.015;
+    }else{
+    interest = 15 + (balance-1000)*0.01;
+    }
+    balance += interest;
+    payment = balance/10;
+    if(balance<10 || payment<10){
+        payment = 10;
+    }
+    cout<<" balance = $"<<fixed<<balance<<" total, $"<<payment<<
+            fixed<<" minimum Payment\n";
+    cout<<"Calculate again? yes/no: ";
+    cin>>answer;
+    }while(answer == "yes");
+    
     //Part 2
     cout<<"\n\nPart 2\n\nLets play 23!";
     int player, computer;
     int toothpicks = 23;
     
     do{
-        cout<<endl<<toothpicks<<" left\nhow many toothpicks would you like to take(1-3)? ";
+        cout<<endl<<toothpicks<<" toothpicks left\nhow many toothpicks would you like to take(1-3)? ";
         cin>>player;
         if(player>3){
-            cout<<"\nNumber must be 3 or less\nhow many toothpicks "
+            cout<<"Number must be 3 or less\nhow many toothpicks "
                     "would you like to take(1-3)? ";
             cin>>player;
         }
         if(player > toothpicks-1){
-            cout<<"\nYou must leave atleast 1 toothpick."<<
+            cout<<"You must leave atleast 1 toothpick."<<
                     " Try again: ";
             cin>>player;     
         }
         toothpicks -= player;
-        cout<<endl<<toothpicks<<" toothpicks left";
+        cout<<toothpicks<<" toothpicks left";
         if(toothpicks>7){
           srand(time(NULL));
           computer = rand()%3+1;
-        }else if(toothpicks<=7 && toothpicks>4){
-            computer=toothpicks-4;
+        }else if(toothpicks<=8 && toothpicks>5){
+            computer=toothpicks-5;
         }else{
             computer=toothpicks-1;
+            if(computer<1 || computer == 4){
+                computer = 1;
+            }
         }
         toothpicks -= computer;
+
+        cout<<"\nThe computer chooses "<<computer<<" toothpicks";
         if(toothpicks < 1){
             cout<<"\nYou win!";
             break;
@@ -75,20 +80,52 @@ int main(int argc, char** argv) {
         if(toothpicks == 1){
             cout<<"\nYou loose";
         }
-        cout<<"\nThe compuer chooses "<<computer<<" toothpicks";
-        cout;
         
     }while(toothpicks>1);
     
+    //Part 3
+    cout<<"\n\nPart 3\n\n";
+    string word;
+    do{
+      cout<<"Please enter a word: ";
+      cin>>word;
+      int flag=0;
+      int size = word.size();
+      for(int i=0;i<(size/2);i++){
+          if( word.substr(i,1)!= word.substr(size-i-1,1) && flag<1){
+            cout<<"\nThis is not a palindrome.";
+            flag++;
+          }
+      }
+      if(flag<1){
+          cout<<"\nThis is a palindrome!";
+      }
+      cout<<"\nWould you like to try another word? yes/no: ";
+      cin>>answer;
+    }while(answer == "yes");
+        
+        
+    //part 4
+    cout<<"\n\nPart 4\n\nPlease enter how many stars you would like per row: ";
+    int stars;
+    cin>>stars;
+    stars++;
+    for(int i=0;i<(stars-2);i+=2){
+        for(int j=0;j<((stars+1+i)-stars);j++){
+            cout<<"*";
+        }
+        cout<<"-";
+        for(int k=0; k<(stars-(i+2));k++){
+            cout<<"*";
+        }
+        cout<<endl;
+   }
     
     
     
     
     
     
-    
-    
-    
-    return 0;
+       return 0;
 }
 
